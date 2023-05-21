324c6299d4f0e07f8798610b65a22ad0a1c11ffb
Varlog Err <varlog.err@gmail.com>
2023-05-21 17:10:16 +0300

    update git ps1

--- core/resources/conf/ps1/git.sh
+++ core/resources/conf/ps1/git.sh
@@ -1,4 +1,4 @@
-# To skip changing changing PS1 to PS1_GIT, paste
+# To skip changing PS1 to PS1_GIT, paste
 #   PS1_GIT="${PS1}"
 # before the current script is sourced, or
 #   PS1="${PS1_ORIGIN}"
@@ -6,20 +6,26 @@
 _iife_ps1_git() {
   unset _iife_ps1_git
 
-  local is_root=false; [[ $(id -u) -eq 0 ]] && is_root=true
-  # define colors
-  local prefix_color=32; ${is_root} && prefix_color=36
-  local branch_color=33; ${is_root} && branch_color=33
-  # format PS1
-  local prefix; prefix="$(
-    printf '\[\033[01;'${prefix_color}'m\]%s %s\[\033[00m\]' '\u@\h' '\w'
-  )"
-  local branch_format=' :\[\033[01;'${branch_color}'m\]%s\[\033[00m\]'
-  local branch; branch="\$( \
-    typeset -F __git_ps1 &>/dev/null && __git_ps1 \"${branch_format}\" \
-  )"
+  declare -A color
+  [ $(id -u) -eq 0 ] && color=(
+    # root colors
+    [prefix]=36
+    [branch]=33
+  ) || color=(
+    # non-root colors
+    [prefix]=32
+    [branch]=33
+  )
 
+  # define formats
+  local branch_format=' :\[\033[01;'${color[branch]}'m\]%s\[\033[00m\]'
+  declare -A format; format=(
+    [prefix]="$(printf '\[\033[01;'${color[prefix]}'m\]%s %s\[\033[00m\]' '\u@\h' '\w')"
+    [branch]='$(__git_ps1 '"'${branch_format}'"' 2>/dev/null)'
+  )
+
+  # format PS1
   PS1_ORIGIN="${PS1_ORIGIN:-${PS1}}"
-  PS1_GIT="${PS1_GIT:-$(printf '[%s%s] ' "${prefix}" "${branch}")}"
+  PS1_GIT="${PS1_GIT:-$(printf -- '[%s%s] ' "${format[prefix]}" "${format[branch]}")}"
   PS1="${PS1_GIT}"
 }; _iife_ps1_git
